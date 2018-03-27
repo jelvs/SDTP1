4a350136d9f3d0c53917bf7fdd9d7a0e50068948
tjfsousa <jelvs13@gmail.com>
2018-03-27 01:50:54 +0100

    add NamenodeServer. ListBlobs implemented with http request

--- src/server/NamenodeServer.java
+++ src/server/NamenodeServer.java
@@ -1,4 +1,34 @@
 package server;
 
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import sys.storage.DatanodeClient;
+import sys.storage.NamenodeClient;
+
+import java.net.InetAddress;
+import java.net.URI;
+
 public class NamenodeServer {
+
+    private static URI baseURI;
+
+    public static void main(String[] args) throws Exception {
+
+
+        //create Server
+        String baseURI = String.format("http://"+ InetAddress.getLocalHost().getHostAddress() +":9091/");
+        System.out.println(baseURI );
+        ResourceConfig config = new ResourceConfig();
+        config.register( new NamenodeClient() );
+
+        JdkHttpServerFactory.createHttpServer( URI.create(baseURI), config);
+
+        System.err.println("Server ready....");
+
+
+
+
+
+
+    }
 }
--- src/sys/storage/LocalBlobStorage.java
+++ src/sys/storage/LocalBlobStorage.java
@@ -1,46 +1,69 @@
 package sys.storage;
 
-import java.util.List;
-
 import api.storage.BlobStorage;
-import api.storage.Datanode;
-import api.storage.Namenode;
+import org.glassfish.jersey.client.ClientConfig;
 import sys.storage.io.BufferedBlobReader;
 import sys.storage.io.BufferedBlobWriter;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocalBlobStorage implements BlobStorage {
-	private static final int BLOCK_SIZE=512;
+    private static final int BLOCK_SIZE = 512;
 
-	Namenode namenode;
-	Datanode[] datanodes;
+    String namenode;
+    String[] datanodes;
 
-	public LocalBlobStorage() {
-		this.namenode = new NamenodeClient();
-		this.datanodes = new Datanode[] { new DatanodeClient() };
-	}
+    public LocalBlobStorage() {
+        this.namenode = "http://localhost:9091";
+        this.datanodes = new String[]{"http://localhost:9999"};
+    }
 
-	@Override
-	public List<String> listBlobs(String prefix) {
-		return namenode.list(prefix);
-	}
+    @Override
+    public List<String> listBlobs(String prefix) {
+        ClientConfig config = new ClientConfig();
+        Client client = ClientBuilder.newClient(config);
 
-	@Override
-	public void deleteBlobs(String prefix) {
-		namenode.list( prefix ).forEach( blob -> {
-			namenode.read( blob ).forEach( block -> {
-				datanodes[0].deleteBlock(block);
-			});
-		});
-		namenode.delete(prefix);
-	}
+        URI baseURI = UriBuilder.fromUri(namenode).build();
+        WebTarget target = client.target(baseURI);
 
-	@Override
-	public BlobReader readBlob(String name) {
-		return new BufferedBlobReader( name, namenode, datanodes[0]);
-	}
+        Response response = target.path("/namenode/list/")
+                .queryParam("prefix", prefix)
+                .request()
+                .get();
 
-	@Override
-	public BlobWriter blobWriter(String name) {
-		return new BufferedBlobWriter( name, namenode, datanodes, BLOCK_SIZE);
-	}
+        if (response.hasEntity()) {
+            List<String> data = response.readEntity(List.class);
+            return data;
+        } else
+            System.err.println(response.getStatus());
+            return new ArrayList<String>();
+
+    }
+
+    @Override
+    public void deleteBlobs(String prefix) {
+        namenode.list(prefix).forEach(blob -> {
+            namenode.read(blob).forEach(block -> {
+                datanodes[0].deleteBlock(block);
+            });
+        });
+        namenode.delete(prefix);
+    }
+
+    @Override
+    public BlobReader readBlob(String name) {
+        return new BufferedBlobReader(name, namenode, datanodes[0]);
+    }
+
+    @Override
+    public BlobWriter blobWriter(String name) {
+        return new BufferedBlobWriter(name, namenode, datanodes, BLOCK_SIZE);
+    }
 }
